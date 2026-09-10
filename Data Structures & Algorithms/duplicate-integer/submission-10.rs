c1ebfb9b9937c1922b322181702c92315859c557
Balagopal <83808261+Balagopal-datascientist@users.noreply.github.com>
2026-09-10 07:58:54 +0530

    Add: duplicate-integer - submission-10

---
+++ Data Structures & Algorithms/duplicate-integer/submission-10.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        use std::collections::HashSet;
+        let mut set : HashSet<i32>= HashSet::new();
+        for i in &nums{
+            if set.contains(i) {
+                return true
+            }
+            println!("{i}");
+            set.insert(*i);
+        }
+    return false;
+    }
+}
